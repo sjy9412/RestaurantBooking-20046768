9e88056b76583e4dae6b1ed3e509b642e2ef8454
jiyeong <sjy9412@gmail.com>
2026-07-02 13:54:08 +0900

    refact: TestableBookingScheduler Mock 객체로 대체(patching)

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -4,7 +4,6 @@ import pytest
 from pytest_mock import MockerFixture
 
 from schedule import Customer, Schedule
-from communication import SmsSender, MailSender
 from booking_scheduler import BookingScheduler
 
 NOT_ON_THE_HOUR = datetime.strptime("2021/03/26 09:05", "%Y/%m/%d %H:%M")
@@ -12,14 +11,6 @@ ON_THE_HOUR = datetime.strptime("2021/03/26 09:00", "%Y/%m/%d %H:%M")
 
 UNDER_CAPACITY = 1
 CAPACITY_PER_HOUR = 3
-
-class TestableBookingScheduler(BookingScheduler):
-    def __init__(self, capacity_per_hour, date_time: str):
-        super().__init__(capacity_per_hour)
-        self._date_time = date_time
-
-    def get_now(self):
-        return datetime.strptime(self._date_time, "%Y/%m/%d %H:%M")
 
 @pytest.fixture
 def customer(mocker: MockerFixture):
@@ -107,15 +98,25 @@ def test_이메일이_있는_경우에는_이메일_발송(booking_scheduler_wit
 
     mail_mock.send_mail.assert_called_once()
 
-def test_현재날짜가_일요일인_경우_예약불가_예외처리(customer):
-    booking_scheduler = TestableBookingScheduler(CAPACITY_PER_HOUR, "2021/03/28 17:00")
+def test_현재날짜가_일요일인_경우_예약불가_예외처리(mocker, customer):
+    mock_get_now = mocker.patch(
+        'booking_scheduler.BookingScheduler.get_now',
+        return_value=datetime.strptime("2021/03/28 17:00", "%Y/%m/%d %H:%M")
+    )
+
+    booking_scheduler = BookingScheduler(CAPACITY_PER_HOUR)
     schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, customer)
 
     with pytest.raises(ValueError):
         booking_scheduler.add_schedule(schedule)
 
-def test_현재날짜가_일요일이_아닌경우_예약가능(customer):
-    booking_scheduler = TestableBookingScheduler(CAPACITY_PER_HOUR, "2024/06/04 17:00")
+def test_현재날짜가_일요일이_아닌경우_예약가능(mocker, customer):
+    booking_scheduler = mocker.patch(
+        'booking_scheduler.BookingScheduler.get_now',
+        return_value=datetime.strptime("2024/06/03 17:00", "%Y/%m/%d %H:%M")
+    )
+
+    booking_scheduler = BookingScheduler(CAPACITY_PER_HOUR)
     schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, customer)
 
     booking_scheduler.add_schedule(schedule)
